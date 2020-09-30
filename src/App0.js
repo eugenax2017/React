a7f6e3f8118cecb66a3e6df6912eb77ba0cb48b0
Eugene <eugenax2017@gmail.com>
2020-09-30 11:25:07 +0200

    30/09/20

--- src/App0.js
+++ src/App0.js
@@ -1,11 +1,8 @@
-//Lazy load
 import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 import Loader from "./Loader";
-import Modal from "./Modal/Modal";
-
-const AddTodo = React.lazy(() => import("./Todo/AddTodo"));
 
 function App() {
   /*const [todos, setTodos] = React.useState([
@@ -18,11 +15,11 @@ function App() {
   const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=15")
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then((response) => response.json())
       .then((todos) => {
         setTodos(todos);
-        setLoading(false);
+        setLoading(false)
       });
   }, []);
 
@@ -57,10 +54,8 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>Things to do</h1>
-        <Modal />
-        <React.Suspense fallback={<p>Loading... </p>}>
-          <AddTodo onCreate={addTodo} />
-        </React.Suspense>
+        <AddTodo onCreate={addTodo} />
+
         {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
